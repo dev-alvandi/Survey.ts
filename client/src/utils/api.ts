ed54c5e5238c4de7ecaab80d10a42b58e1ecd902
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2023-09-06 13:01:10 +0200

    Live server's address is added!

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,5 +1,5 @@
 const PORT = '5000';
 
-export const BASE_API_URL: string = `http://localhost:${PORT}/api`;
-export const BASE_BACKEND_URL: string = `http://localhost:${PORT}`;
-export const BASE_API_IMAGE_url: string = `http://localhost:${PORT}`;
+export const BASE_API_URL: string = `https://survey-mern-clone.onrender.com:${PORT}/api`;
+export const BASE_BACKEND_URL: string = `https://survey-mern-clone.onrender.com:${PORT}`;
+export const BASE_API_IMAGE_url: string = `https://survey-mern-clone.onrender.com:${PORT}`;
